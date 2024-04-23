e6c72907c2b05cc3dc690afc57015f8677f6a4d2
Prashant Salve <128687203+prashant04071995@users.noreply.github.com>
2024-04-23 15:11:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+  const counterElement = document.getElementById('counter');
+  const incrementBtn = document.getElementById('incrementBtn');
+
+  let counterValue = 0;
+
+  incrementBtn.addEventListener('click', () => {
+    alert(`Counter value before increment: ${counterValue}`);
+    counterValue++;
+    counterElement.textContent = counterValue;
+  });
+});
